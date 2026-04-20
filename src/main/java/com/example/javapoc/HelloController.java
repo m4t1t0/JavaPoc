cbd3712819673bb0b56e0f4f3c3e66ccd6243c2f
Rafael Matito <rafa.matito@gmail.com>
2026-04-20 09:22:29 +0200

    feat: Add logger

--- src/main/java/com/example/javapoc/HelloController.java
+++ src/main/java/com/example/javapoc/HelloController.java
@@ -1,14 +1,19 @@
 package com.example.javapoc;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    private static final Logger log = LoggerFactory.getLogger(HelloController.class);
+
     @GetMapping("/")
     public String hello() {
-        String s = "Hello World!";
+        String s = "Hello World chiquitito!";
+        log.info("s = {}", s);
 
         return String.format("%s", s);
     }
